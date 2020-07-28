4cd8f37a3ac5fea5a52058eda42c8a822a68028d
AMBAL-DAMBAL <ablai2602@gmail.com>
2020-07-28 13:35:28 +0600

    Initial Commit

---
+++ Kaktus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kaktus
+//
+//  Created by ablai erzhanov on 7/28/20.
+//  Copyright © 2020 ablai erzhanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
